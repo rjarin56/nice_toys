396414cc36cc5a569e71a6a60c15fe2bb279e37d
“rjarin56” <reniejarin@gmail.com>
2017-11-18 17:47:53 -0600

    Added foreign_key: :category_id to product model file.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, foreign_key: :category_id
   validates :product_name, :category_id, :stock_quantity, presence: true
 end
--- db/migrate/20171118233805_change_products_category_id_id.rb
+++
@@ -1,5 +0,0 @@
-class ChangeProductsCategoryIdId < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :products, :category_id_id
-  end
-end
