eee82de6664bdf160903e1aafa3fb8a1e314ee95
“rjarin56” <reniejarin@gmail.com>
2017-11-18 16:33:16 -0600

    Forgot to put the end after the change_table do while loop in the up and down methods.

--- db/migrate/20171118222612_change_category_id.rb
+++ db/migrate/20171118222612_change_category_id.rb
@@ -2,9 +2,11 @@ class ChangeCategoryId < ActiveRecord::Migration[5.1]
   def up
     change_table :products do |t|
       t.change :category_id, :string
+    end
   end
   def down
     change_table :products do |t|
       t.change :category_id, :integer, foreign_key: true
+    end
   end
 end
