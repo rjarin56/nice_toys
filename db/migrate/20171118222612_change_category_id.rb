7e25a0d4e7f713b85445674ee200eb3cec632921
“rjarin56” <reniejarin@gmail.com>
2017-11-18 16:31:26 -0600

    Modified the migration file to add two methods, 'up' method will have a change_table :products do |t| t.change :category_id, :string. 'down' method will have a change_table :products do |t| t.change :category_id, :integer, foreign_key: true. Will check if this will make the category_id into integer and the foreign key.

--- db/migrate/20171118222612_change_category_id.rb
+++ db/migrate/20171118222612_change_category_id.rb
@@ -1,4 +1,10 @@
 class ChangeCategoryId < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    change_table :products do |t|
+      t.change :category_id, :string
+  end
+  def down
+    change_table :products do |t|
+      t.change :category_id, :integer, foreign_key: true
   end
 end
