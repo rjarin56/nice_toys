26acffc8a92b6f2836edd6413f2f9e1849f99386
“rjarin56” <reniejarin@gmail.com>
2017-11-20 23:06:55 -0600

    Added a before_action :retrieve_categories in application_controller.rb file. Added a private method of retrieve_categories with the variable @categories = Category.all. I will try getting this to work again, but first I want to implement some css

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,9 +1,9 @@
 class CategoryController < ApplicationController
-  def index
-    @categories = Category.order(:name)
-  end
-
-  def show
-    @category = Category.includes(:products).find(params[:id])
-  end
+  # def index
+  #   @categories = Category.order(:name)
+  # end
+  #
+  # def show
+  #   @category = Category.includes(:products).find(params[:id])
+  # end
 end
