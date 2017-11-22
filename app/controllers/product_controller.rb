ad4112ec22d1ec6814c4cbbe47d535df498a0a5f
“rjarin56” <reniejarin@gmail.com>
2017-11-21 23:14:25 -0600

    Installed imagemagick on the homebrew and had to change some settings to get it to work. Finally got the paperclip gem to work properly after getting the imagemagick to work.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,6 @@
 class ProductController < ApplicationController
   def index
-    @products = Product.order([:product_name])
+    @products = Product.order([:product_name]).page(params[:page]).per(5)
     # @categories = Category.all
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates :product_name, :category_id, :stock_quantity, presence: true
   validates :image, :attachment_presence => true
-  validates_attachment :image, :presence => true, :content_type => { :content_type => "image/jpeg" }, :size => { :in => 0..100.kilobytes }
+  validates_attachment :image, :presence => true, :content_type => { :content_type => "image/jpeg" }, :size => { :in => 0..100.megabytes }
 
   def self.search(search, category)
     if category != ''
